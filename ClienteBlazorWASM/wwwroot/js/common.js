dcfc2a7d7bc25a6acc5127ba99bdc14a275b87b9
Jorge <jorge---9511@hotmail.com>
2026-01-22 13:07:08 -0600

    feat: Se implementa sweet alert, y toastr

---
+++ ClienteBlazorWASM/wwwroot/js/common.js
@@ -0,0 +1,37 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operación correcta", { timeOut: 10000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operación fallida", { timeOut: 10000 });
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification',
+            message,
+            'success'
+        );
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification',
+            message,
+            'error'
+        );
+    }
+}
+
+window.MostrarModalConfirmacionBorrado = () => {
+    const modal = document.getElementById('modalConfirmacionBorrado');
+    const bsModal = new bootstrap.Modal(modal);
+    bsModal.show();
+}
+
+window.OcultarModalConfirmacionBorrado = () => {
+    const modal = document.getElementById('modalConfirmacionBorrado');
+    const bsModal = bootstrap.Modal.getInstance(modal);
+    bsModal.hide();
+}
