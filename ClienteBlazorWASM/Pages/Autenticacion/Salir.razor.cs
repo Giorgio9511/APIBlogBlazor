5c857f3e9c274ebade333c719e69eacf0e69eb37
Jorge <jorge---9511@hotmail.com>
2026-01-28 11:22:33 -0600

    feat: Se restringen links en nav principal

---
+++ ClienteBlazorWASM/Pages/Autenticacion/Salir.razor.cs
@@ -0,0 +1,20 @@
+﻿using ClienteBlazorWASM.Servicios.IServicio;
+using Microsoft.AspNetCore.Components;
+
+namespace ClienteBlazorWASM.Pages.Autenticacion
+{
+    public partial class Salir
+    {
+        [Inject]
+        public IServicioAutenticacion servicioAutenticacion { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await servicioAutenticacion.Salir();
+            navigationManager.NavigateTo("/");
+        }
+    }
+}
